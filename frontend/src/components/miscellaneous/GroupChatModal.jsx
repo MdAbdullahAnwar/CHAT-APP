f53607e938e79e1289ec8c6604d2b2b075141497
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-12-12 23:29:33 +0530

    Fix all case-sensitive import paths for deployment

--- frontend/src/components/miscellaneous/GroupChatModal.jsx
+++ frontend/src/components/miscellaneous/GroupChatModal.jsx
@@ -18,8 +18,8 @@ import { Users, Search, Sparkles } from "lucide-react";
 import axios from "axios";
 import { useState } from "react";
 import { ChatState } from "../../Context/ChatProvider";
-import UserBadgeItem from "../userAvatar/UserBadgeItem";
-import UserListItem from "../userAvatar/UserListItem";
+import UserBadgeItem from "../UserAvatar/UserBadgeItem";
+import UserListItem from "../UserAvatar/UserListItem";
 
 const GroupChatModal = ({ children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
